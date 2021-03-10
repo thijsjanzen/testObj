7d118ba91d770ceb43709fc2ab988cf74efa231e
Thijs Janzen <thijsjanzen@gmail.com>
2021-03-10 10:15:31 +0100

    clean up code

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -1,38 +1,20 @@
 
-#include <thread>
-#include <chrono>
-
 #include <Rcpp.h>
 using namespace Rcpp;
 
-void force_output() {
-  //  std::this_thread::sleep_for(std::chrono::nanoseconds(100));
-  std::this_thread::sleep_for(std::chrono::milliseconds(300));
-  R_FlushConsole();
-  R_ProcessEvents();
-  R_CheckUserInterrupt();
-}
-
-
-
 NumericVector predict_gam_from_r(const List& gam_obj,
                                  const std::vector<double>& pars) {
-  Function f("predict_val"); // can not be global
+  static Function f("predict_val"); // can not be global
   Rcpp::DataFrame df = Rcpp::DataFrame::create(Named("x0") = pars[0],
                                                Named("x1") = pars[1],
                                                Named("x2") = pars[2],
                                                Named("x3") = pars[3]);
-//  Rcout << "created df\n";
 
   return f(gam_obj, df);
 }
 
 // [[Rcpp::export]]
 NumericVector predict_cpp(const List& gam_obj, const NumericVector& pars) {
- /* Rcout << "inside get_predict\n"; force_output();
-  for (int i = 0; i < pars.size(); ++i) {
-    Rcout << pars[i] << "\n"; force_output();
-  }*/
   NumericVector out = predict_gam_from_r(gam_obj,
                                          std::vector<double>(pars.begin(), pars.end()));
   return out;
--- tests/example.R
+++ tests/example.R
@@ -1,26 +1,39 @@
+# generate some testing data (this is copied from the predict.gam example
+# in the gam documentation)
 n <- 200
 sig <- 2
 dat <- mgcv::gamSim(1,n = n, scale = sig)
 b <- mgcv::gam(y~s(x0) + s(I(x1^2)) + s(x2) + offset(x3),
                data = dat)
+
+# create some fake dataframe that might look like our future data:
 newd <- data.frame(x0 = 0.5,
                    x1 = 0.5,
                    x2 = 0.5,
                    x3 = 0.5)
+
+
+# use the mgcv prediction function
 pred <- mgcv::predict.gam(b, newd)
 
+
+# use the R function in this package (full R based)
 predict_val(b, newd)
+
+# use the walkaround via Rcpp:
+predict_cpp(b, c(0.5, 0.5, 0.5, 0.5))
+
 
 # note that predict_cpp only accepts an unnamed vector
 # as.vector(newd) does not work (and crashes R).
-predict_cpp(b, c(0.5, 0.5, 0.5, 0.5))
-
 # this does work:
 predict_cpp(b, as.numeric(as.vector(newd)))
 
+# all values should be the same:
 all.equal(mgcv::predict.gam(b, newd)[[1]],
           predict_cpp(b, c(0.5, 0.5, 0.5, 0.5)))
 
+# how is the speed? the walkaround via cpp is slightly slower (but not much):
 a <- microbenchmark::microbenchmark(predict_val(b, newd),
                                     predict_cpp(b, c(0.5, 0.5, 0.5, 0.5)))
 
